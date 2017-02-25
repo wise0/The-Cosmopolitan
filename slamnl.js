4882a05f51c83554dc25aaa4fe47546faaad3ae1
wise0 <wisevertix@gmail.com>
2017-02-25 13:51:14 -0800

    Update slamnl.js

--- slamnl.js
+++ slamnl.js
@@ -4,7 +4,7 @@ const creds = require("./creds.json")
 
 client.login(creds.token);
 
-<client>.user.setGame("EDM/Tropical/House");
+client.user.setGame("EDM/Tropical/House");
 
 client.on("ready", () => {
 	client.channels.get(creds.channelid).join().then(connection => {
